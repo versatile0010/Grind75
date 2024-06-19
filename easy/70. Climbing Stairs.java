e9471038505b493399cb7477dd7c4992b8079329
Lee Jaehyeon <wogusn@gmail.com>
2024-06-19 21:35:44 +0900

    Create 70. Climbing Stairs.java

---
+++ easy/70. Climbing Stairs.java
@@ -0,0 +1,22 @@
+/*
+
+DP approach.
+
+Time Complexity:
+O(n) where n is a input value.
+
+
+*/
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+        int [] dp = new int [n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
