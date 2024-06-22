db810d8eee66cd2ef7a42ada59d57e5ea7c0c071
Lee Jaehyeon <wogusn@gmail.com>
2024-06-22 10:08:34 +0900

    Create 232. Implement Queue using Stacks(approach1).java

---
+++ easy/232. Implement Queue using Stacks(approach1).java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class MyQueue {
+    
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+    private int front;
+
+    public MyQueue() {
+        s1 = new Stack<>();  
+        s2 = new Stack<>(); 
+    }
+    
+    public void push(int x) {
+        if(s1.isEmpty()){
+            front = x;
+        }
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        s2.push(x);
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+    }
+    
+    public int pop() {
+        int x = s1.pop();
+        if(!s1.isEmpty()){
+            front = s1.peek();
+        }
+        return x;
+    }
+    
+    public int peek() {
+        return front;
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
